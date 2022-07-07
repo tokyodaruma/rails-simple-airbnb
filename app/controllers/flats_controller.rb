44dd973c0156be324badc293a8d2edc9804fb813
Zach Carter <zacarter93@gmail.com>
2022-07-07 21:18:34 +0900

    progress so far

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,36 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path, notice: 'Flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(
+      :name,
+      :address,
+      :description,
+      :price_per_night,
+      :number_of_guests
+    )
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Tokyo',
+  address: 'Minamikamata, Ota-ku, Tokyo',
+  description: 'LOLOLOL OMG ARE YOU SERIOUS?!',
+  price_per_night: 100_000,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Dark & Tight Garage Flat London',
+  address: '101 Rockcreek Lake Road',
+  description: 'Dip your big toe in acid',
+  price_per_night: 0,
+  number_of_guests: 1_000
+)
+
+Flat.create!(
+  name: 'Deep & Bodacious Rails',
+  address: 'XOXO Railway',
+  description: 'It was a cold and windy night - ravens flying overhead...',
+  price_per_night: 75,
+  number_of_guests: 0
+)
+
+Flat.create!(
+  name: 'Dirty & Icky, Ewie Gooie Madness',
+  address: 'Take me home tonight',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
